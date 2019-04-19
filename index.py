0e0f723b93ff56419c33f63a2ddbe6b77da85ab7
paul-ml <pauljoe.george@monstar-lab.com>
2019-04-19 16:54:46 +0900

    basic alexa skill to listen to joke

---
+++ index.py
@@ -0,0 +1,37 @@
+import logging
+import random
+from random import randint
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question, session
+import datetime
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+@ask.launch
+def launch():
+  print("I came here")
+  welcome_msg = render_template('welcome')
+  return question(welcome_msg)
+
+@ask.intent("YesIntent")
+def positive_response():
+  jokes = [
+	" Never trust math teachers who use graph paper. They are always plotting something.",
+	" What do you get when you cross a snake with a tasty dessert? A pie-thon!",
+	"Why is Peter Pan flying all the time? He Neverlands!"
+	]
+  joke = random.choice(jokes)
+  print("joke is.................. " + joke)
+  joke_template = render_template('fun', joke=joke)
+  return statement(joke_template)
+
+@ask.intent("NoIntent")
+def negative_response():
+    response= render_template('no_response')
+    return statement(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
